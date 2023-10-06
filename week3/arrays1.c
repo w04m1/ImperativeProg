a32212aa5ef0d1c9bcd66e5020cbbeb25444f6f3
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-10-06 14:19:08 +0200

    done arrays1 for week3

---
+++ week3/arrays1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter how many zeros you want: ");
+    scanf("%d", &n);
+    int zeros[n];
+    for (int i = 0; i < n; i++)
+    {
+        zeros[i] = 0;
+    }
+    printf("Here are your zeros: [");
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+        {
+            printf("%d", zeros[i]);
+            break;
+        }
+        printf("%d, ", zeros[i]);
+    }
+    printf("]\n");
+}
