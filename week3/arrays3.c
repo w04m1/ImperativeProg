13994a8bd5d941f78d7cfc1b5c312ee820242f7d
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2023-10-06 14:28:37 +0200

    done arrays3 for week3

---
+++ week3/arrays3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int nums[9] = {
+        8,
+        2,
+        5,
+        30,
+        20,
+        16,
+        12,
+        50,
+        40,
+    };
+    int greatest = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        if (greatest < nums[i])
+        {
+            greatest = nums[i];
+        }
+    }
+    printf("The greatest number is: %d\n", greatest);
+    return 0;
+}
